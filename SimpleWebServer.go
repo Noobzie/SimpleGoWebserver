96cc2c4abb072c42e6630c44f4a1c0909c8c175d
Noobzie <mohaas121@hotmail.com>
2019-11-18 23:06:28 +0100

    Commenting

--- SimpleWebServer.go
+++ SimpleWebServer.go
@@ -8,28 +8,28 @@ import (
 )
 
 func main() {
-	var randomWords = []string{"Raspberry", "ESP32", "Lopy4", "Arduino"}
-	var ids = []int{0}
+	var randomWords = []string{"Raspberry", "ESP32", "Lopy4", "Arduino"} //Slice of strings
+	var ids = []int{0}                                                   //Slice of int's
 
-	http.HandleFunc("/4", handler)
+	http.HandleFunc("/4", handler) //Call function outside of main
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) { //responds to localhost:8080
 		log.Println(r.Method, r.URL.Path)
-		fmt.Fprintln(w, "Hello, world!")
+		fmt.Fprintln(w, "Hello, world!") //Print to browser
 	})
 
-	http.HandleFunc("/getNewId", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/getNewId", func(w http.ResponseWriter, r *http.Request) { //responds to localhost:8080/getNewId, gives the last value in the slice and adds last value + 1 to the slice
 		log.Println(r.Method, r.URL.Path)
-		id := ids[len(ids)-1]
-		fmt.Fprintln(w, id)
-		ids = append(ids, id+1)
+		id := ids[len(ids)-1]   //Find the last int in slice
+		fmt.Fprintln(w, id)     //Print to browser
+		ids = append(ids, id+1) //Add new int on the end of the slice
 	})
 
-	http.HandleFunc("/3", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/3", func(w http.ResponseWriter, r *http.Request) { //responds to localhost:8080/3 with a JSON example
 		log.Println(r.Method, r.URL.Path)
-		b, _ := json.Marshal(randomWords)
-		s := string(b)
-		fmt.Fprintln(w, s)
+		b, _ := json.Marshal(randomWords) //Convert slice to JSON byte array
+		s := string(b)                    //JSON byte array to string
+		fmt.Fprintln(w, s)                //Print to browser
 	})
 
 	log.Println("Listening on port 8080")
@@ -38,7 +38,7 @@ func main() {
 	}
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func handler(w http.ResponseWriter, r *http.Request) { //responds to localhost:8080/4 and finds the parameters in the url.		Example: localhost:8080/?key=CO2%20655	<- finds CO2 and 655
 
 	keys, ok := r.URL.Query()["key"]
 
@@ -51,5 +51,5 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	// we only want the single item.
 	key := keys[0]
 
-	log.Println("Url Param 'key' is: " + string(key))
+	log.Println("Url Param 'key' is: " + string(key)) //Print to console
 }
